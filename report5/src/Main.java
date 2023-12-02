6a559979e7a443ac9181fbc7ca2a82213dbd876a
Aka <e235758@ie.u-ryukyu.ac.jp>
2023-12-02 16:41:16 +0900

    2nd commit

--- report5/src/Main.java
+++ report5/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args){
+        try{
         String empty =null;
         System.out.println(empty.length());
-
+        }
+        catch(NullPointerException e){
+            System.out.println(e.getMessage());
+            System.out.println("NullPointerException : " + e.getMessage());
+            e.printStackTrace();
+            System.out.println("NullPointerException が発生しました！");
+        }
     }
 }
